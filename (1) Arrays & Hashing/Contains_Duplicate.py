7dbc86d98c4e4d34111604f10c545813387a8198
Nicolas Martinez <137114121+nicom0202@users.noreply.github.com>
2023-07-28 11:26:14 -0700

    Delete Contains_Duplicate.py
    deleting because added it to sub folder

--- (1) Arrays & Hashing/Contains_Duplicate.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        elements = set()
-        for i in range(len(nums)):
-            if nums[i] in elements:
-                return True
-            elements.add(nums[i])
-
-        return False
